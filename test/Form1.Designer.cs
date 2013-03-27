f1d936495e473e3d260258a9d19f2ca1e423049a
Adam Johns <robert.adam.johns@gmail.com>
2013-03-27 10:52:57 +0800

    -initial commit

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Set Timer For";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Minutes";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(155, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start Timer";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Window;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(88, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ReminderTimer";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(44, 80);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.numericUpDown1.Size = new System.Drawing.Size(66, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Reminder Message";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(172, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "(Optional)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(121, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 23);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "00:00";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Window;
+            this.button2.Location = new System.Drawing.Point(41, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 34);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Cancel Timer";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(293, 265);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ReminderTimer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+
+
+
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        // minutesChosen is combobox variable. minutesTicking is initialized to minutesChosen but decremented to be 
+        // displayed on screen as each minute passes
+        decimal minutesChosen, minutesTicking;
+
+        // Variable for displaying seconds on screen
+        decimal secondsTicking = 0;
+
+        // Optional custom reminder message
+        string reminderMessage;
+
+        // Flag for setting default minutes value to 5 in case it isn't changed
+        bool minutesChanged = false;
+
+        // Flag for testing if a timer has already been started (so we don't start another one if one is alraeady going)
+        bool alreadyStarted = false;
+
+        // Flag for testing if alarm file was found correctly
+        bool alarmSoundFound = false;
+
+        // Create a timer to call alerting event when minutes are up, and a countdown timer that calls a function 
+        // every second for updating the countdown display
+        //Timer timer = new Timer();
+        Timer countdown = new Timer();
+
+        // Sound file variable
+        System.Media.SoundPlayer alarmSound;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Call setupAlarmSound() to set up alarm file
+            setupAlarmSound();
+        }
+
+        /*****************************************************************************************************************
+         * setupAlarmSound() checks to make sure the alarm file is found, so that a runtime error will not occur with
+         * a file not found error.
+         ****************************************************************************************************************/
+        void setupAlarmSound()
+        {
+            if (File.Exists(@"C:\Program Files (x86)\ReminderTimer\sweet_wake_up.wav"))
+            {
+                alarmSound = new System.Media.SoundPlayer(@"C:\Program Files (x86)\ReminderTimer\sweet_wake_up.wav");
+                alarmSoundFound = true;
+            }
+            else if (File.Exists(@"C:\Program Files\ReminderTimer\sweet_wake_up.wav"))
+            {
+                alarmSound = new System.Media.SoundPlayer(@"C:\Program Files\ReminderTimer\sweet_wake_up.wav");
+                alarmSoundFound = true;
+            }
+            else
+            {
+                MessageBox.Show("Alarm sound file not found. Program will still work, but it won't play alarm sound", "ReminderTimer");
+            }
+        }
+
+        /*****************************************************************************************************************
+         * button1_Click is called when start timer button is pressed. It checks minutesChanged flag to determine whether
+         * or not to use default 5 minute value. It also checks the alreadyStarted flag to ensure the timer code is not
+         * called while a timer is already running. It sets up a timer_Tick and countdown_Tick functions, and creates time
+         * intervals for both starts timer.
+         ****************************************************************************************************************/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Don't allow user to change time while timer is running
+            numericUpDown1.Enabled = false;
+
+            if (alreadyStarted == false)
+            {
+                alreadyStarted = true;
+                if (minutesChanged == false)
+                {
+                    minutesChosen = Convert.ToInt32(5);
+                    minutesTicking = 5;
+                }
+
+                // Everytime timer ticks, timer_Tick will be called.
+                //timer.Tick += new EventHandler(timer_Tick);
+                countdown.Tick += new EventHandler(countdown_Tick);
+
+                // Convert ms to actual minutes
+                //timer.Interval = Convert.ToInt32(minutesChosen) * (1000 * 60);
+
+                // Countdown function called every second to update countdown timer on screen
+                countdown.Interval = 1000;
+
+                //timer.Enabled = true;
+                countdown.Enabled = true;
+                //timer.Start();
+                countdown.Start();
+            }
+
+        }
+
+        /********************************************************************************************** 
+         * This function handles the timer expiration. Once the timer expires, the alarm sound begins
+         * to play, a message box will pop up, and the timer will be stopped to avoid another timer 
+         * interval going off. Also, stop the alarm sound when user clicks OK, and restart the program
+         * to get back to initial state.
+         **********************************************************************************************/
+        void timerExpires()
+        {
+            // Only try to play the file if the file was found
+            if (alarmSoundFound == true)
+                alarmSound.Play();
+
+            // Display default reminder message if the user doesn't enter a custom message
+            string msg;
+            if (reminderMessage == "" || reminderMessage == null)
+            {
+                msg = "Timer finished";
+            }
+            else
+            {
+                msg = reminderMessage;
+            }
+
+            DialogResult dlgResult = MessageBox.Show(new Form() { TopMost = true }, msg, "ReminderTimer");
+            if (dlgResult == DialogResult.OK)
+            {
+                // Only try to stop the sound if alarm file was found
+                if (alarmSoundFound == true)
+                    alarmSound.Stop();
+                Application.Restart();
+            }
+        }
+
+        /****************************************************************************
+         * countdown_Tick handles the countdown timer display updates on the screen. 
+         * It is called every second to update the timer shown every second.
+         ****************************************************************************/
+        void countdown_Tick(object sender, EventArgs e)
+        {
+            // New string variables for displaying the values to the screen
+            string secTicks = secondsTicking.ToString();
+            string minTicks = minutesTicking.ToString();
+
+            // Logic for making sure time is shown in xx:xx format
+            if (minutesTicking == 9)
+                minTicks = "09";
+            if (minutesTicking == 8)
+                minTicks = "08";
+            if (minutesTicking == 7)
+                minTicks = "07";
+            if (minutesTicking == 6)
+                minTicks = "06";
+            if (minutesTicking == 5)
+                minTicks = "05";
+            if (minutesTicking == 4)
+                minTicks = "04";
+            if (minutesTicking == 3)
+                minTicks = "03";
+            if (minutesTicking == 2)
+                minTicks = "02";
+            if (minutesTicking == 1)
+                minTicks = "01";
+            if (minutesTicking == 0)
+                minTicks = "00";
+
+            if (secondsTicking == 9)
+                secTicks = "09";
+            if (secondsTicking == 8)
+                secTicks = "08";
+            if (secondsTicking == 7)
+                secTicks = "07";
+            if (secondsTicking == 6)
+                secTicks = "06";
+            if (secondsTicking == 5)
+                secTicks = "05";
+            if (secondsTicking == 4)
+                secTicks = "04";
+            if (secondsTicking == 3)
+                secTicks = "03";
+            if (secondsTicking == 2)
+                secTicks = "02";
+            if (secondsTicking == 1)
+                secTicks = "01";
+            if (secondsTicking == 0)
+                secTicks = "00";
+
+            // Display the time in label6
+            label6.Text = minTicks + ":" + secTicks;
+
+            secondsTicking--;
+
+            // Logic for resetting seconds and decrementing minutes
+            if (secondsTicking < 0)
+            {
+                secondsTicking = 59;
+                minutesTicking--;
+            }
+
+            // Timer has expired so stop the countdown and display 00:00
+            if (minutesTicking < 0)
+            {
+                minutesTicking = 0;
+                secondsTicking = 0;
+                countdown.Stop();
+                timerExpires();
+            }
+
+
+        }
+
+        /***************************************************************************************
+         * When numeric value changed this function sets minutes to equal the value in the box.
+         ***************************************************************************************/
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // This flag is used to tell button1_click to override the default value of 5 with 
+            // the value received from numeric up/down box
+            minutesChanged = true;
+            minutesChosen = numericUpDown1.Value;
+
+            // Initialize minutesTicking to what was chosen
+            minutesTicking = minutesChosen;
+        }
+
+        /*************************************************************************************
+         * Get custom reminder message from user (optional)
+         *************************************************************************************/
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            reminderMessage = textBox1.Text;
+        }
+
+        /*************************************************************************************
+         * Restart the application to return to init state if user cancels timer by clicking
+         * "Cancel Timer" button (button2)
+         *************************************************************************************/
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+
+
+    }
+}
